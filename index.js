a227b6ef22194c21d8afc5e96aa55578cfcd8cfa
rogerio-damasio <raugusto2512@gmail.com>
2026-09-16 14:05:37 -0300

    Initialize Express server in index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+JS
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+res.send("Rodando no Render");
+});
+app.listen(PORT, () => {
+console.log('Servidor rodando na porta ${PORT}');
+});
